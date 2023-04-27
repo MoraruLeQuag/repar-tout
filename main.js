08bdb87294f9db3d80e1ba9d69b5965f554b85a0
MoraruLeQuag <118672238+MoraruLeQuag@users.noreply.github.com>
2023-04-27 14:36:42 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+const folderPlus = document.querySelector('.bx-folder-plus');
+            folderPlus.addEventListener('click', () => {
+              window.location.href = 'file:///C:/Users/Guillaume/Documents/ReparToutWebSite/devis.html';
+            });
+const laptop = document.getElementById('laptopIMG');
+            laptop.addEventListener('click', () => {
+              window.location.href = 'file:///C:/Users/Guillaume/Documents/ReparToutWebSite/laptop.html';
+            });
+const PC = document.getElementById("PcIMG");
+            PC.addEventListener('click', () => {
+              window.location.href = 'file:///C:/Users/Guillaume/Documents/ReparToutWebSite/PC.html';
+            });
+const smartphone = document.getElementById('smartphoneIMG');
+            smartphone.addEventListener('click', () => {
+              window.location.href = 'file:///C:/Users/Guillaume/Documents/ReparToutWebSite/Smartphone.html';
+            });
+        
